9f6f8ba556cd33839ab9bfd2423e4094a6dc2819
jdegiovanni <jdegiovanni@workfusion.com>
2022-09-22 15:34:57 -0500

    add trash button

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -5,6 +5,11 @@ const plusSign = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=
     "  <path d=\"M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z\"/>\n" +
     "</svg>";
 
+const trashIcon = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-trash\" viewBox=\"0 0 16 16\">\n" +
+    "  <path d=\"M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z\"/>\n" +
+    "  <path fill-rule=\"evenodd\" d=\"M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z\"/>\n" +
+    "</svg>";
+
 function reloadStuff() {
     allStuff = undefined;
     loadStuff();
@@ -40,6 +45,8 @@ function createStuffListItem(stuff, parentList) {
     listItem.innerText = toString(stuff);
     listItem.appendChild(createSpanSpace());
     listItem.appendChild(createPlusButton());
+    listItem.appendChild(createSpanSpace());
+    listItem.appendChild(createTrashButton());
     listItem.appendChild(createChildList(stuff.nestedStuff));
     parentList.append(listItem);
 }
@@ -68,6 +75,14 @@ function createPlusButton() {
     button.setAttribute("class", "bi bi-plus-circle");
     button.innerHTML = plusSign;
     button.setAttribute("onclick", "addRowBelow(this.parentElement)");
+    return button;
+}
+
+function createTrashButton() {
+    const button = document.createElement("i");
+    button.setAttribute("class", "bi bi-trash");
+    button.innerHTML = trashIcon;
+    button.setAttribute("onclick", "console.log('delete me sometime')");
     return button;
 }
 
